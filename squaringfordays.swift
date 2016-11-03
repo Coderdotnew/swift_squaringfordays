8bb6d1ed1b8dadc6a6b21ae8b4d42b11007523bb
Stephanholly <sholly@usc.edu>
2016-11-03 14:02:39 -0700

    first commit

---
+++ squaringfordays.swift
@@ -0,0 +1,23 @@
+// Squaring for Days
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]
+
+
+// create a function that iterates through the array and doubles each item in the array.
+
+
+
+
+
+// create a function that iterates throught the array, to output "You're a wizard, _________" for every name in the array.
+
+var wizards = ["Harry", "Ron", "Dumbledore", "Grindelwald", "Draco"]
+
+
+
+
+
+
+
+
+
